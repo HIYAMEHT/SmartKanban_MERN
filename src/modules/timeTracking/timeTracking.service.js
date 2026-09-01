a2ab059a2e36bc11810d4d2e46d8740d6595d3e6
Tanvi Nirwan <tanvinirwan123@gmail.com>
2026-09-01 09:34:21 +0530

    feat: add timer stop logic

--- src/modules/timeTracking/timeTracking.service.js
+++ src/modules/timeTracking/timeTracking.service.js
@@ -1,22 +1,22 @@
 const apiError = require('../../utils/apiError') ;
-const timeLogModel = require('../timeTracking/timeTracking.model')
+const TimeLog = require('../timeTracking/timeTracking.model')
 const Task = require("../task/task.model");
 const {CONFLICT, NOT_FOUND} = require('../../utils/httpStatus');
 
 const startTimer = async(userId,taskId) => {
     //why we are using userId not taskId bc => a person cannot have multiple timers 
-   const ExistingTimer = await timeLogModel.findOne({user : userId, status : 'Running'}) ;
+   const ExistingTimer = await TimeLog.findOne({user : userId, status : 'Running'}) ;
     if(ExistingTimer){
         return apiError(CONFLICT,"The timer is already running for a task") 
     }
     //find if the task really exists 
-    const taskExist = await taskModel.findById(taskId) ;
+    const taskExist = await Task.findById(taskId) ;
     if(!taskExist){
         return apiError(NOT_FOUND,"Task not found") ;
     }
     //create the timer log 
 
-    const timeLog = await Task.create({
+    const timeLog = await TimeLog.create({
         user : userId ,
         task : taskId,
         startTime :new Date(), //how to find the current tiem 
@@ -26,4 +26,24 @@ const startTimer = async(userId,taskId) => {
     return timeLog ;
 }
 
-module.exports = {startTimer} ;
+
+const stopTimer = async (userId) => {
+   // find running timer
+    const runningTimer = await TimeLog.findOne({user:userId,status:'Running'}) ;
+    // if not found → error
+    if(!runningTimer){
+        return apiError(NOT_FOUND,"Running timer for this task is not found") ;
+    }   
+   // set endTime
+   runningTimer.endTime = new Date() ;
+   // calculate duration
+   runningTimer.durationSeconds = (runningTimer.endTime-runningTimer.startTime)/1000 ;
+   // set status
+   runningTimer.status = 'Completed'
+   // save
+   await runningTimer.save() ;
+   // return
+   return runningTimer ;
+}
+
+module.exports = {startTimer,stopTimer} ;
